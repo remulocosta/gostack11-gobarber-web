104ed5360f14885e4c30e400b8961223fa676911
Remulo Costa <remulo.costa@gmail.com>
2020-07-12 22:07:29 -0300

    created test from SignIn page

--- src/__tests__/pages/SingIn.spec.tsx
+++ src/__tests__/pages/SingIn.spec.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import SingIn from '../../pages/SignIn';
 
+const mockedHistoryPush = jest.fn();
+
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SingIn Page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(<SingIn />);
+    const { getByPlaceholderText, getByText } = render(<SingIn />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
